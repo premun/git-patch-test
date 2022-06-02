9b70e235dc844e2b5422e3805d884f3432d96260
Premek Vysoky <premek.vysoky@microsoft.com>
2022-06-02 15:13:22 +0200

    Improve build-containers to not cd

--- build-containers.ps1
+++ build-containers.ps1
@@ -2,11 +2,9 @@
 
 docker build --rm -t vmr-base -f base.Dockerfile .
 
-Set-Location tests
-docker build --rm -t vmr-case-1 -f case-1.Dockerfile .
-docker build --rm -t vmr-case-2 -f case-2.Dockerfile .
-docker build --rm -t vmr-case-3 -f case-3.Dockerfile .
-docker build --rm -t vmr-case-4 -f case-4.Dockerfile .
-docker build --rm -t vmr-case-5 -f case-5.Dockerfile .
-docker build --rm -t vmr-case-6 -f case-6.Dockerfile .
-Set-Location ..
+docker build --rm -t vmr-case-1 -f tests/case-1.Dockerfile tests
+docker build --rm -t vmr-case-2 -f tests/case-2.Dockerfile tests
+docker build --rm -t vmr-case-3 -f tests/case-3.Dockerfile tests
+docker build --rm -t vmr-case-4 -f tests/case-4.Dockerfile tests
+docker build --rm -t vmr-case-5 -f tests/case-5.Dockerfile tests
+docker build --rm -t vmr-case-6 -f tests/case-6.Dockerfile tests
